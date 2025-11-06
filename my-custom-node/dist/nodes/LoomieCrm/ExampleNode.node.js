0abcc59c08a71c3f2d20ff62e8efe54324c6d2be
Diogo <dapereira@inf.ufsm.br>
2025-11-06 12:53:57 -0300

    feat: logo loomie svg branco

--- my-custom-node/dist/nodes/LoomieCrm/ExampleNode.node.js
+++ my-custom-node/dist/nodes/LoomieCrm/ExampleNode.node.js
@@ -15,7 +15,7 @@ class ExampleNode {
         this.description = {
             displayName: 'LoomieCRM Node',
             name: 'exampleNode',
-            icon: 'file:LoomieCrm/loomiecrm.svg',
+            icon: 'file:./logo-loomie.svg',
             group: ['transform'],
             version: 1,
             description: 'Node com funções da API LoomieCRM',
